9c539785b9ffbe17c11d9d550a864ce7350ebf20
Mamoona-shaikh23 <bscs2312118@szabist.pk>
2025-05-09 05:27:37 +0000

    Create get methods

--- HRAPI/index.js
+++ HRAPI/index.js
@@ -14,19 +14,43 @@ app.get('/',async(req , res)=>{
          res.status(500).json({ERROR :err.message});
     }
 });
-
-app.get('/country', async(req , res)=>{
+app.get('/department', async(req , res)=>{
     try{
-         const result = await pool.query('select* from countries');
+         const result = await pool.query('select d.department_name, count(e.employee_id) from departments d left join employees e on d.department_id = e.department_id group by d.department_name');
          res.json(result.rows);
     }catch(err){
         res.status(500).json({Error:err.message});
     }
-})
+});
 
-app.get('/region' , async(req , res)=>{
+app.get('/location', async(req , res)=>{
     try{
-        const result = await pool.query('select* from regions');
+         const result = await pool.query(`select * from location`);
+         res.json(result.rows);
+    }catch(err){ 
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/country', async(req , res)=>{
+    try{
+         const result = await pool.query('select count(country_id) from countries');
+         res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+app.get('/region', async(req , res)=>{
+    try{
+         const result = await pool.query('select count(region_id) from regions');
+         res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/job_history' , async(req , res)=>{
+    try{
+        const result = await pool.query(`select `);
         res.json(result.rows);
     }catch(err){
         res.status(500).json({Error:err.message});
@@ -35,7 +59,7 @@ app.get('/region' , async(req , res)=>{
 
 app.get('/employee' ,async(req ,res)=>{
     try{
-        const result = await pool.query('select* from employees');
+        const result = await pool.query( ` select e.first_name, e.last_name, e.employee_id, j.job_title from employees e join jobs j on e.job_id = j.job_id join departments d on e.department_id = d.department_id join locations l on d.location_id = l.location_id where l.city = 'Toronto' `);
         res.json(result.rows);
     }catch(err){
         res.status(500).json({Error:err.message});
